8d1434f1487a65bd2beea89ea77d21ddce322b11
CodeForGhost <ilikeabdul7@gmail.com>
2023-03-20 20:39:23 +0530

    user login part done and jwt token generated

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const { User, validate } = require("../models/user");
 const router = express.Router();
 
@@ -16,6 +17,9 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send("User already registered!");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ["_id", "name", "email"]));
