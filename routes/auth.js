36b8eed2241d079884e4d2acaf267f288bf6084a
CodeForGhost <ilikeabdul7@gmail.com>
2023-03-20 22:00:44 +0530

    generate token function moved user model and became single source of truth

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,7 @@ const express = require("express");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
-const jwt = require("jsonwebtoken");
 
-const config = require("config");
 const { User } = require("../models/user");
 const router = express.Router();
 
@@ -18,9 +16,9 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  const token = user.generateAuthToken();
 
-  res.send(token);
+  res.header("x-auth-token", token).send(token);
 });
 
 function validate(req) {
